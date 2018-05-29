3fb5d7a390308e707d89bd4bd3175f015e5a9bce
mumumushi <yuanchenbiao@outlook.com>
2018-05-29 11:44:19 +0800

    First commit

--- app/src/main/java/com/example/moneybox/MainActivity.java
+++ app/src/main/java/com/example/moneybox/MainActivity.java
@@ -79,7 +79,6 @@ public class MainActivity extends AppCompatActivity
             getDataFromLEWEI50(lastRequestDate.substring(0, lastRequestDate.indexOf(" ")));
         }
 
-        Log.d(TAG, "onCreate: test 33333333");
 
         //后期计划添加splash启动页面
         //可收缩的Toolbar的显示设置
